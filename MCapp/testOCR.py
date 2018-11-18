3c4863e0a47afe82db4f35d9d6f86c4e7e0670b5
dantetun <dante.zayyar@gmail.com>
2018-11-18 15:14:11 +0000

    simple OCR and test case for OCR

---
+++ MCapp/testOCR.py
@@ -0,0 +1,11 @@
+import unittest
+import simpleOCR
+
+class TestOCR(unittest.TestCase):
+    def test_simple_ocr(self):
+        test_bob = simpleOCR.image_to_text("bob.jpg")
+        expected = "While Bob ate an apple was in the basket."
+        self.assertEqual(test_bob,expected)
+
+if __name__ == "__main__":
+    unittest.main()
