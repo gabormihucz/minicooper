76d0a6efac81929f364db997fa80b070d0546fe2
Nestor Skiadas <2258257s@student.gla.ac.uk>
2018-11-21 11:30:53 +0000

    Update testOCR.py

--- MCapp/testOCR.py
+++ MCapp/testOCR.py
@@ -3,7 +3,7 @@ import simpleOCR
 
 class TestOCR(unittest.TestCase):
     def test_simple_ocr(self):
-        test_bob = simpleOCR.image_to_text("bob.jpg")
+        test_bob = simpleOCR.image_to_text(r"bob.jpg")
         expected = "While Bob ate an apple was in the basket."
         self.assertEqual(test_bob,expected)
 
