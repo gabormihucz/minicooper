6fc5d848bd87b42ed5b03643f23580a9714fff48
Nestor Skiadas <2258257s@student.gla.ac.uk>
2018-11-27 11:35:25 +0000

    models work

--- MCweb/MCwebDjango/mcwebapp/models.py
+++ MCweb/MCwebDjango/mcwebapp/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class PdfFile(models.Model):
     file_name = models.CharField(max_length = 200)
     upload_date = models.DateTimeField('date uploaded')
+
+    def __str__(self):
+        return self.file_name
