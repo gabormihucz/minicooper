477c58e8caa87c138b5d38ba5b752587adda70b0
Nestor Skiadas <2258257s@student.gla.ac.uk>
2018-11-27 13:10:30 +0000

    views, static files, guide for templates, mentioning

--- MCweb/MCwebDjango/mcwebapp/views.py
+++ MCweb/MCwebDjango/mcwebapp/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    # to work with templates:
+    # - make a context_dict (standard python dictionary) to pass the data into the template
+    # - return render(request, 'mcwebapp/[html file].html', context=context_dict)
+    # note that the directory string above points to MCweb/MCwebDjango/templates as root.
+    #
+    # to access the values from inside the HTML:
+    # - {{ [dictionary key] }}
+    #
+    # to use static files (like the logo image):
+    # - write {% load staticfiles %} right before <html>
+    # - for a picture static/images/cat/jpg
+    #       <img src="{% static "images/cat.jpg" %}" alt="picture of a cat">
+    # @2277073z
+
     return HttpResponse("Hello world.")
