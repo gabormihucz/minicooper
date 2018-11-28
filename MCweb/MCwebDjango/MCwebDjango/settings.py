688425c88e1df5aa86415e4c93f426e2101613a2
Nestor Skiadas <2258257s@student.gla.ac.uk>
2018-11-28 12:28:03 +0000

    settings and forms for user functionality

--- MCweb/MCwebDjango/MCwebDjango/settings.py
+++ MCweb/MCwebDjango/MCwebDjango/settings.py
@@ -92,9 +92,11 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'OPTIONS': {'max_similarity': 0.7,}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 6,}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
