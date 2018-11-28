5aa7087938bbc95718daa04f8bb34c6633930a70
Nestor Skiadas <2258257s@student.gla.ac.uk>
2018-11-28 12:57:58 +0000

    models and views for user registration, template next

--- MCweb/MCwebDjango/mcwebapp/views.py
+++ MCweb/MCwebDjango/mcwebapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import UserForm
 
 def index(request):
     # to work with templates:
@@ -17,3 +18,25 @@ def index(request):
     # @2277073z
 
     return render(request,'mcwebapp/index.html',{})
+
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            registered = True
+        else:
+            print(user_form.errors)
+    else:
+        user_form = UserForm()
+
+    context_dict = {'user_form': user_form
+                    'registered': registered}
+    return render(request, 'mcwebapp/register.html', context_dict)
