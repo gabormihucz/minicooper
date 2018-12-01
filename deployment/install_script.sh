3fd3e7ba8702b988fded6057a404839c00842115
Nestor Skiadas <2258257s@student.gla.ac.uk>
2018-12-01 14:59:01 +0000

    git pull in deployment

--- deployment/install_script.sh
+++ deployment/install_script.sh
@@ -7,3 +7,7 @@ apt-get install git
 apt-get install ssh
 ssh-add darryl_key
 eval $(ssh-agent -s)
+
+git init
+git add remote origin git@stgit.dcs.gla.ac.uk:tp3-2018-cs15/dissertation.git
+git pull origin master
