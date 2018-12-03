b4510d174e6e00f42c14b3e1d6456bef1314c0dd
Gabor Mihucz <2239495m@student.gla.ac.uk>
2018-12-03 23:41:36 +0000

    adding tests about views and user profiles

--- MCweb/MCwebDjango/mcwebapp/tests.py
+++ MCweb/MCwebDjango/mcwebapp/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+import unittest
+from mcwebapp.models import *
 
 # Create your tests here.
+
+from django.urls import reverse
+class IndexViewTests(TestCase):
+	def test_index_view(self):
+
+		response = self.client.get(reverse(''))
+		self.assertEqual(response.status_code, 200)
+
+class TestUserProfiles(unittest.TestCase):
+    def setUp(self):
+        self.u1 = User.objects.create(username='user1')
+        self.up1 = UserProfile.objects.create(user=self.u1)
+
+    def TestUserNameExists(self):
+        self.assertEqual(User.objects.get(username = 'user1').username, 'user1')
+
+    def tearDown(self):
+        self.up1.delete()
+        self.u1.delete()
