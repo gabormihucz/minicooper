9ac2344063e69d62c040af96da7cc90e71a1ff5d
Gabor Mihucz <2239495m@student.gla.ac.uk>
2018-12-04 01:08:32 +0000

    adding test for login

--- MCweb/MCwebDjango/mcwebapp/tests.py
+++ MCweb/MCwebDjango/mcwebapp/tests.py
@@ -22,3 +22,15 @@ class TestUserProfiles(unittest.TestCase):
     def tearDown(self):
         self.up1.delete()
         self.u1.delete()
+
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+    def test_login(self):
+        # send login data
+        response = self.client.post('/accounts/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
