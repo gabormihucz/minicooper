dd141571d2843ff1894986fe082428cc4651d9c9
Gabor Mihucz <2239495m@student.gla.ac.uk>
2018-12-12 16:43:45 +0000

    Update models.py - commenting out the PdfFile model to see if this is the reason CI does not build

--- MCweb/MCwebDjango/mcwebapp/models.py
+++ MCweb/MCwebDjango/mcwebapp/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+"""
 class PdfFile(models.Model):
     file_name = models.CharField(max_length = 200)
     upload_date = models.DateTimeField('date uploaded')
 
     def __str__(self):
         return self.file_name
-
+"""
 class UserProfile(models.Model):
 	# This line is required. Links UserProfile to a User model instance.
 	user = models.OneToOneField(User, on_delete=models.CASCADE,)
