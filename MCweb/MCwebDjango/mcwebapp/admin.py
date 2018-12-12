90338eb846586c875f5c252ed7670d945a48d64d
Gabor Mihucz <2239495m@student.gla.ac.uk>
2018-12-12 16:46:41 +0000

    Update admin.py - commenting out references to PdfFile to see if CI builds

--- MCweb/MCwebDjango/mcwebapp/admin.py
+++ MCweb/MCwebDjango/mcwebapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from mcwebapp.models import UserProfile
-from mcwebapp.models import PdfFile
+"""from mcwebapp.models import PdfFile
 
 admin.site.register(PdfFile)
+
+commenting this out temporarily
+"""
 
 # Register your models here.
