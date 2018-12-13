821dab04e11cfe2930d61f14a78c1a54a0e9b385
Nestor Skiadas <2258257s@student.gla.ac.uk>
2018-12-13 14:07:44 +0000

    indentation fix in models

--- MCweb/MCwebDjango/mcwebapp/models.py
+++ MCweb/MCwebDjango/mcwebapp/models.py
@@ -17,38 +17,38 @@ class UserProfile(models.Model):
 class PDFFile(models.Model):
     name = models.CharField(max_length = 200)
     upload_date = models.DateTimeField('date uploaded')
-	file_ = models.FileField(upload_to='pdfFiles/')
-	json = models.OneToOneField(JSONFile)
+    file_ = models.FileField(upload_to='pdfFiles/')
+    json = models.OneToOneField(JSONFile)
 
     def __str__(self):
         return self.name
 
 
 class JSONFile(models.Model):
-	name = models.CharField(max_length=30)
-	upload_date = models.DateTimeField('date uploaded')
-	file_ = models.FileField(upload_to='jsonFiles/')
-	json = models.ForeignKey(PDFFile)
+    name = models.CharField(max_length=30)
+    upload_date = models.DateTimeField('date uploaded')
+    file_ = models.FileField(upload_to='jsonFiles/')
+    json = models.ForeignKey(PDFFile)
 
-	def __str__(self):
+    def __str__(self):
         return self.name
 
 
 class TemplateFile(models.Model):
-	name = models.CharField(max_length=30)
-	upload_date = models.DateTimeField('date uploaded')
-	file_ = models.FileField(upload_to='templateFiles/')
-	user = models.ForeignKey(User)
+    name = models.CharField(max_length=30)
+    upload_date = models.DateTimeField('date uploaded')
+    file_ = models.FileField(upload_to='templateFiles/')
+    user = models.ForeignKey(User)
 
-	def __str__(self):
+    def __str__(self):
         return self.name
 
 
 class MatchPattern(models.Model):
-	name = models.CharField(max_length=30)
-	regex = models.CharField(max_length=60)
-	template = models.ForeignKey(TemplateFile)
+    name = models.CharField(max_length=30)
+    regex = models.CharField(max_length=60)
+    template = models.ForeignKey(TemplateFile)
 
-	def __str__(self):
+    def __str__(self):
         return self.name
 
