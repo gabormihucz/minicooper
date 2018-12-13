da109d34176bf8dbb14d7f89eccfb50912b76415
Gabor Mihucz <2239495m@student.gla.ac.uk>
2018-12-13 19:10:11 +0000

    Changing tests.py to accommodate redirecting anonymous users to the login page

--- MCweb/MCwebDjango/mcwebapp/tests.py
+++ MCweb/MCwebDjango/mcwebapp/tests.py
@@ -6,23 +6,22 @@ from django.utils import timezone
 
 # Create your tests here.
 
-class IndexViewTests(TestCase):
-    def test_index_view(self):
-
+class IndexViewTestAnonymous(TestCase):
+    def test_index_view_anonymous(self):
         response = self.client.get(reverse(''))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)  # anonymous user should be redirected, hence get HTTP 302
 
-class TestUserProfiles(unittest.TestCase):
+class IndexViewTestLoggedIn(TestCase):
     def setUp(self):
-        self.u1 = User.objects.create(username='user1')
-        self.up1 = UserProfile.objects.create(user=self.u1)
-
-    def TestUserNameExists(self):
-        self.assertEqual(User.objects.get(username = 'user1').username, 'user1')
-
-    def tearDown(self):
-        self.up1.delete()
-        self.u1.delete()
+        self.credentials = {
+            'username': 'testuser2',
+            'password': 'secret2'}
+        User.objects.create_user(**self.credentials)
+    def test_login_logged_in(self):
+        # send login data
+        self.client.post('/accounts/login/', self.credentials, follow=True)
+        response = self.client.get(reverse(''))
+        self.assertEqual(response.status_code, 200) # logged in user should get HTTP 200 from the homepage
 
 class LogInTest(TestCase):
     def setUp(self):
@@ -35,6 +34,19 @@ class LogInTest(TestCase):
         response = self.client.post('/accounts/login/', self.credentials, follow=True)
         # should be logged in now
         self.assertTrue(response.context['user'].is_active)
+
+
+class TestUserProfiles(unittest.TestCase):
+    def set_up(self):
+        self.u1 = User.objects.create(username='user1')
+        self.up1 = UserProfile.objects.create(user=self.u1)
+
+    def TestUserNameExists(self):
+        self.assertEqual(User.objects.get(username = 'user1').username, 'user1')
+
+    def tearDown(self):
+        self.up1.delete()
+        self.u1.delete()
 
 class ModelTest(unittest.TestCase):
     def pdfFileTest(self):
