994f438dfa1ad20353e01372f94d8ab45830c9c7
Gabor Mihucz <2239495m@student.gla.ac.uk>
2018-12-14 00:30:34 +0000

    Update admin.py - registering the rest of the models

--- MCweb/MCwebDjango/mcwebapp/admin.py
+++ MCweb/MCwebDjango/mcwebapp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from mcwebapp.models import PDFFile, UserProfile
+from mcwebapp.models import *
 
 admin.site.register(PDFFile)
-
+admin.site.register(JSONFile)
 admin.site.register(UserProfile)
-
-
+admin.site.register(TemplateFile)
+admin.site.register(MatchPattern)
 # Register your models here.
