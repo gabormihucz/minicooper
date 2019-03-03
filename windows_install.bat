d489e3dc33847d05839dafd21b0b4554f7255316
Nestor Skiadas <2258257s@student.gla.ac.uk>
2019-03-03 12:58:08 +0000

    Update windows_install.bat

--- windows_install.bat
+++ windows_install.bat
@@ -1,12 +1,16 @@
+ECHO OFF
+
 ECHO "Deploying project..."
 
 ECHO "Installing Python 3 (pip3 included)..."
 powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.7.2/python-3.7.2-amd64.exe -OutFile python3.exe"
 start /W python3.exe
+DEL python3.exe
 
 ECHO "Installing Tesseract OCR..."
 powershell -Command "Invoke-WebRequest https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w64-setup-v4.0.0.20181030.exe -OutFile tesseract.exe"
 start /W tesseract.exe
+DEL tesseract.exe
 
 ECHO "Installing virtualenv..."
 pip3 install virtualenv
@@ -26,5 +30,6 @@ python manage.py makemigrations
 ECHO "Starting server..."
 python manage.py runserver
 
+CD ..\..
 :End
 cmd /k
